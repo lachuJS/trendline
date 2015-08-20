6675d14fa560b398bf38ada32ec0510d7111616d
Lakshmi Narayanan <lakshminarayanan2060@gmail.com>
2015-08-20 18:28:16 +0530

    -crawl on npm start -cards with link -css ed

--- public/js/getlines.js
+++ public/js/getlines.js
@@ -7,26 +7,48 @@ function query(){
 }
 io.on('gotlines',function (rows){
 	 if (rows.length==0) {
-	 	var link=document.getElementById('more');
-	 	link.removeAttribute('href');
-	 	link.innerHTML='...'
+	 	if (count==0) {
+	 		count++;
+	 		io.emit('getlines',count);
+	 	}
+	 	else{
+		 	var link=document.getElementById('more');
+		 	link.innerHTML=''
+	 	}
 	 }
 	 else {
 	 	for (var i = 0; i < rows.length; i++) {
-	 		var line=document.createElement('div');
-	 		line.className='line';
+	 		var card=document.createElement('div')
+	 		card.className='card';
+	 		
+	 		var card_content=document.createElement('div');
+	 		card_content.className='card-content black-text';
 
-		 	var title=document.createElement('span');
-			title.className='title';
-	 		title.appendChild(document.createTextNode(rows[i]['title']));
+			var title=document.createElement('div');
+			var bold=document.createElement('strong');
+			bold.appendChild(document.createTextNode(rows[i]['title']));
+			title.appendChild(bold);
+			title.className='card-title black-text';
 
-		 	var desc=document.createElement('span');
-			desc.className='desc';
-	 		desc.appendChild(document.createTextNode(rows[i]['description']));
+		 	var desc=document.createElement('p');
+		 	var desc_text=document.createTextNode(rows[i]['description']);
+		 	desc.appendChild(desc_text);
 
-	 		line.appendChild(title);
-	 		line.appendChild(desc);
-	 		document.getElementById('line-container').appendChild(line);
+			var link=document.createElement('a');
+			link.setAttribute('href',rows[i]['url']);
+			link.setAttribute('target','_blank');
+			link.className='black-text';
+			link.setAttribute('style','text-decoration:underline');
+			link.appendChild(document.createTextNode(rows[i]['source']));
+			link_div=document.createElement('div');
+			link_div.className='card-action';
+			link_div.appendChild(link);
+
+	 		card_content.appendChild(title);
+	 		card_content.appendChild(desc);
+	 		card.appendChild(card_content);
+	 		card.appendChild(link_div);
+	 		document.getElementById('234').appendChild(card);
 
 	 	};
 	 }		 	
