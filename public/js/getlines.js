a3b5208a26291ff0c255d6061b26e93ce73f82cb
Lakshmi Narayanan <lakshminarayanan2060@gmail.com>
2015-08-21 22:57:31 +0530

    -simpler -wider

--- public/js/getlines.js
+++ public/js/getlines.js
@@ -25,43 +25,41 @@ io.on('gotlines',function (rows){
 	 else {
 	 	for (var i = 0; i < rows.length; i++) {
 	 		var card=document.createElement('div')
-	 		card.className='card';
-	 		
+	 		card.className='card-panel';
+
 	 		var card_content=document.createElement('div');
-	 		card_content.className='card-content black-text';
+	 		card_content.className='black-text';
 
 			var title=document.createElement('div');
 			var bold=document.createElement('strong');
 			bold.appendChild(document.createTextNode(rows[i]['title']));
 			title.appendChild(bold);
-			title.className='card-title black-text';
+			title.className='black-text';
+			title.setAttribute('style','font-size:25px');
 
 		 	var desc=document.createElement('p');
 		 	var desc_text=document.createTextNode(rows[i]['description']);
 		 	desc.appendChild(desc_text);
+		 	desc.setAttribute('style','text-align:justify');
 
-			var url=document.createElement('strong');
-			
+			var url=document.createElement('strong');	
 			var link=document.createElement('a');
+			link.className='teal-text';
 			link.setAttribute('href',rows[i]['url']);
 			link.setAttribute('target','_blank');
-			link.className='teal-text';
 			
 			url.appendChild(document.createTextNode(rows[i]['source']));
 			link.appendChild(url);
-			link_div=document.createElement('div');
-			link_div.className='card-action';
-			link_div.appendChild(link);
 
 	 		card_content.appendChild(title);
 	 		card_content.appendChild(desc);
+	 		card_content.appendChild(link);
 	 		card.appendChild(card_content);
-	 		card.appendChild(link_div);
 	 		document.getElementById('234').appendChild(card);
 	 	};
 	 	//replace animation with link
 	 	var progress=document.getElementById('progress');
 	 	progress.setAttribute('style','display:none');
-	 	document.getElementById('more').innerHTML='<strong>get more lines.<strong>';
+	 	document.getElementById('more').innerHTML="<strong>get more lines.</strong>";
 	 }		 	
 });
